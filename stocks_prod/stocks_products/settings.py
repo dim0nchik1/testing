745d2c7a7b8f638de3dd08be5bbbca8638db0b70
dim0nchik <dzhikon@bk.ru>
2024-06-19 22:36:47 +0300

    1

--- stocks_prod/stocks_products/settings.py
+++ stocks_prod/stocks_products/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,8 +84,8 @@ DATABASES = {
         'NAME': 'stocks_products',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 89268563508
+        'USER': '',
+        'PASSWORD':''
 
     }
 }
@@ -128,8 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
