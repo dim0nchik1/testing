e463cc663d94f5b16fb525ebb6abb488a9258c86
dim0nchik <dzhikon@bk.ru>
2024-06-20 01:00:59 +0300

    GHJ

--- stocks_prod/stocks_products/settings.py
+++ stocks_prod/stocks_products/settings.py
@@ -83,18 +83,15 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-	    'USER': os.getenv('DB_USER'),
-	    'PASSWORD': os.getenv('DB_PASSWORD')
+DATABASES = dict(default={
+    'ENGINE': os.getenv('DB_ENGINE'),
+    'NAME': os.getenv('DB_NAME'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD')
 
-
-    }
-}
+})
 
 
 # Password validation
